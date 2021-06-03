c2e138cfbc24105671e009ad8233b85fb68bbf50
bobagby78 <bobagby78@gmail.com>
2021-06-03 09:11:04 -0500

    more intellij probs. need to restart again

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -5,7 +5,7 @@ public class Event {
     private String eventName;
     private String eventDescription;
 
-    public Event(String name, String description) {
+    public Event(String eventName, String eventDescription) {
         this.eventName = eventName;
         this.eventDescription = eventDescription;
     }
